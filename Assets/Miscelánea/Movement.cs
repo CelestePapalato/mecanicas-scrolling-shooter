37efb468c76d743c5176b4a39cc37dec0e73861c
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-05-14 23:48:14 -0300

    Cambiado algoritmo de movimiento

--- Assets/Miscelánea/Movement.cs
+++ Assets/Miscelánea/Movement.cs
@@ -13,13 +13,19 @@ public class ExtendedMaths
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0, .15f)] float smoothTime;
     [SerializeField] float maxSpeed;
     [SerializeField] float acceleration;
     [SerializeField] float decceleration;
-
+    [SerializeField]
     float currentMaxSpeed;
+    [SerializeField]
     float currentAcceleration;
+    [SerializeField]
     float currentDecceleration;
+    [SerializeField]
+    float currentSpeed = 0;
 
     public float MaxSpeed { get => maxSpeed; set { currentMaxSpeed = Mathf.Max(maxSpeed, value); } }
     public float Acceleration { get => acceleration; set { currentAcceleration = Mathf.Max(maxSpeed, value); } }
@@ -28,6 +34,7 @@ public class Movement : MonoBehaviour
 
     Vector2 currentDirection = Vector2.zero;
     Vector2 currentVelocity = Vector2.zero;
+    Vector2 DAMP_currentVelocity;
 
     public Vector2 Direction
     {
@@ -54,26 +61,38 @@ public class Movement : MonoBehaviour
 
     private void move()
     {
-        Vector2 movement = Vector2.zero;
-        Vector2 targetSpeed = currentDirection * currentMaxSpeed;
-        float difference = targetSpeed.magnitude - currentVelocity.magnitude;
-        if (!ExtendedMaths.Approximately(difference, 0, 0.0000001f))
+        Vector2 targetVelocity = currentDirection * currentMaxSpeed;
+        float _acceleration = 0;
+        float difference = targetVelocity.magnitude - currentSpeed;
+        if (!ExtendedMaths.Approximately(difference, 0, 0.0001f))
         {
-            float _acceleration = 0;
-            if (difference < 0)
-            {
-                _acceleration = Mathf.Max(-currentDecceleration * Time.fixedDeltaTime, difference);
-            }
-            else
+            if (difference > 0)
             {
                 _acceleration = Mathf.Min(currentAcceleration * Time.fixedDeltaTime, difference);
             }
-            difference = 1/difference;
-            movement = targetSpeed - currentVelocity;
-            movement *= difference * _acceleration;
+            else if (difference < 0)
+            {
+                _acceleration = Mathf.Max(-currentDecceleration * Time.fixedDeltaTime, difference);
+            }
         }
-        currentVelocity += movement;
-        rb.velocity += movement;
-    }
+        currentSpeed += _acceleration;
+        Vector2 targetSpeed = currentDirection * currentSpeed * Time.fixedDeltaTime;
+        if (targetVelocity.magnitude > 0)
+        {
+            //float t = currentSpeed / targetVelocity.magnitude;
+            //currentVelocity = Vector2.Lerp(currentVelocity, targetVelocity, t);
+            if(currentVelocity != Vector2.zero)
+            {
+                currentVelocity = Vector2.SmoothDamp(currentVelocity, currentDirection.normalized, ref DAMP_currentVelocity, smoothTime);
+            }
+            else
+            {
+                currentVelocity = currentDirection.normalized;
+            }
 
+            //currentVelocity = Vector2.Lerp(currentVelocity, currentDirection, smoothTime * Time.fixedDeltaTime);
+        }
+        Debug.Log(currentVelocity * currentSpeed);
+        rb.MovePosition(rb.position + currentVelocity * currentSpeed * Time.deltaTime);
+    }
 }
